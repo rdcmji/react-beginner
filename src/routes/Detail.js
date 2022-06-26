b6bc049a020ece484b1d3a3a24840ed63da6320d
김시내 <sinae@sinae-MacBookPro.local>
2022-06-26 17:53:27 +0900

    add first version

---
+++ src/routes/Detail.js
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import {useParams} from "react-router-dom";
+
+function Detail(){
+    const {id} = useParams();
+    const [loading, setLoading] = useState(true);
+    const [movie, setMovie] = useState([]);
+    const getMovie = async() => {
+        const json = await(
+            await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+        ).json();
+        setMovie(json.data.movie);
+        setLoading(false);
+    };
+    useEffect(() => {
+        getMovie();
+    }, []);
+    return <div>
+    <h1>Detail</h1>
+    {loading? <h1>Loading...</h1> :(
+    <div>
+        <img src={movie.medium_cover_image} alt=""/>
+        <h2>
+            {movie.title}
+        </h2>
+        <p>{movie.description_full}</p>
+        <ul>{movie.genres.map((g)=>(<li key={g}>{g}</li>))}</ul>
+    </div>
+    )}
+  </div>;
+}
+
+export default Detail;
